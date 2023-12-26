e0f56ec0a6aaf29b270106fe4062f67011eab946
omsingh4459 <105961576+omsingh4459@users.noreply.github.com>
2023-12-26 19:47:05 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,9 @@
+const express = require('express')
 const { bgRed } = require('colors')
 const mongoose = require('mongoose')
+
+const app = express()
+const PORT = process.env.PORT || 8080
 const connectDB = async()=>{
     try {
         await mongoose.connect(process.env.MONGO_URL)
@@ -8,5 +12,9 @@ const connectDB = async()=>{
         console.log(`MONGOconnect error ${error}`.bgRed.white);
     }
 }
-
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("listening for requests");
+    })
+})
 module.exports = connectDB;
