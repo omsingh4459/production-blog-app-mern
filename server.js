6f4c8ae9b6ded2964757d7b6e4b90fd51b33bb50
omsingh4459 <105961576+omsingh4459@users.noreply.github.com>
2023-12-27 00:40:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ const corsOptions ={
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
+app.options('*',cors())
 app.use(cors(corsOptions));
 // app.use(cors())
 app.use(express.json())//parse data to json formta sent by client
