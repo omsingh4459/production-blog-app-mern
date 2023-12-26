c009ba38df670fa8df0f4d2f64cd5765a8145e74
omsingh4459 <105961576+omsingh4459@users.noreply.github.com>
2023-12-26 19:25:35 +0530

    code added
    blog-app mern

---
+++ routes/blogRoutes.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const { getAllBlogsController, createBlogController, updateBlogController, getBlogByIdController, deleteBlogController, userBlogController } = require('../controllers/blogController')
+
+//router object
+const router = express.Router()
+
+//routes
+//get all blogs || GET
+router.get('/all-blog', getAllBlogsController)
+
+//POST || create blog
+router.post('/create-blog', createBlogController)
+
+//put || update blog
+router.put('/update-blog/:id', updateBlogController)
+
+//GET || single blog details
+router.get('/get-blog/:id', getBlogByIdController)
+//delete blog || DELETE
+
+router.delete('/delete-blog/:id', deleteBlogController)
+
+//GET || user blog
+router.get('/user-blog/:id', userBlogController)
+module.exports = router
---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const colors = require('colors')
+const dotenv = require('dotenv')
+const path = require("path");
+const connectDB = require('./config/db')
+
+
+//env config
+dotenv.config()
+
+//router import
+const userRoutes = require('./routes/userRoutes')
+const blogRoutes = require('./routes/blogRoutes')
+//mongodb connection
+connectDB();
+
+//rest object
+const app = express()
+
+//midllewares
+app.use(cors())
+app.use(express.json())//parse data to json formta sent by client
+app.use(morgan('dev')) //show url which will be hit by consolw
+
+//routes
+app.use('/api/v1/user', userRoutes);
+app.use('/api/v1/blog', blogRoutes);
+
+
+//static files
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+//PORT
+const PORT = process.env.PORT || 8080
+//listen
+app.listen(PORT, ()=>{
+    console.log(`Server running on ${process.env.DEV_MODE} port no. ${PORT}`.bgCyan.white);
+})
