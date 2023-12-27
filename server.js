7864b078df813a3e6051ae158b241c8b65b39fb9
omsingh4459 <105961576+omsingh4459@users.noreply.github.com>
2023-12-27 15:06:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,16 +20,20 @@ const blogRoutes = require('./routes/blogRoutes')
 const app = express();
 const PORT = process.env.PORT || 8080
 //midllewares
-const corsOptions = {
-  origin:"https://good-plum-meerkat-coat.cyclic.app",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",};
+
+const connectDB = async()=>{
+    try {
+        await mongoose.connect(process.env.MONGO_URL+';')
+        console.log(`Connected to MongoDB database ${mongoose.connection.host}`.bgMagenta.white);
+    } catch (error) {
+        console.log(`MONGOconnect error ${error}`.bgRed.white);
+    }
+}
 
 
-app.use(cors(corsOptions));
-// app.use(cors())
 app.use(express.json())//parse data to json formta sent by client
 app.use(morgan('dev')) //show url which will be hit by consolw
-
+app.use(cors({credentials: true}))
 //routes
 app.use('/api/v1/user', userRoutes);
 app.use('/api/v1/blog', blogRoutes);
@@ -41,47 +45,9 @@ app.use(express.static(path.join(__dirname, "./client/build")));
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
-app.options("*", (req, res) => {
-  console.log("preflight");
-  if (
-    req.headers.origin === "https://badmintown.onrender.com" &&
-    allowMethods.includes(req.headers["access-control-request-method"]) &&
-    allowHeaders.includes(req.headers["access-control-request-headers"])
-  ) {
-    console.log("pass");
-    return res.status(204).send();
-  } else {
-    console.log("fail");
-  }
-  });
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://good-plum-meerkat-coat.cyclic.app"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Private-Network", true);
-  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-  res.setHeader("Access-Control-Max-Age", 7200);
 
-  next();
-});
-const connectDB = async()=>{
-    try {
-        await mongoose.connect(process.env.MONGO_URL+';')
-        console.log(`Connected to MongoDB database ${mongoose.connection.host}`.bgMagenta.white);
-    } catch (error) {
-        console.log(`MONGOconnect error ${error}`.bgRed.white);
-    }
-}
+
+
 
 
 //listen
