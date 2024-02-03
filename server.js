845113b19b0af4da90cb196dc61bd48287794f3a
omsingh4459 <105961576+omsingh4459@users.noreply.github.com>
2024-02-03 13:27:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -44,7 +44,9 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 //routes
+app.use('/api/v1/user', cors(corsOptions)); 
 app.use('/api/v1/user', userRoutes);
+app.use('/api/v1/blog', cors(corsOptions)); 
 app.use('/api/v1/blog', blogRoutes);
 
 
